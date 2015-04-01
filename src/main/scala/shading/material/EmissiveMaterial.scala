f4345830d711ee587c5545ae1b61c865d666c4f2
BeardedPlatypus <mwtegelaers@gmail.com>
2015-04-01 12:50:23 +0200

    Add EmissiveMaterial

--- src/main/scala/shading/material/EmissiveMaterial.scala
+++ src/main/scala/shading/material/EmissiveMaterial.scala
@@ -1,5 +1,14 @@
 package com.beardedplatypus.shading.material
 
-abstract class EmissiveMaterial(receivesShadow: Boolean) extends Material(receivesShadow = receivesShadow) {
+import com.beardedplatypus.shading.{Color, RayResult}
+import com.beardedplatypus.world.Scene
 
+class EmissiveMaterial(val ls: Double,
+                       val c: Color) extends Material(false) {
+
+
+  override def shade(rayResult: RayResult, scene: Scene): Color  = {
+    if ((rayResult.normal dot rayResult.ray.direction.inverted) >= 0.0) c * ls
+    else Color.black
+  }
 }
