102acae61ec9e94cf723fe78c75a6fb6090b09fa
BeardedPlatypus <mwtegelaers@gmail.com>
2015-04-03 20:34:27 +0200

    Update EmissiveMaterial shading
    * Added Le

--- src/main/scala/shading/material/EmissiveMaterial.scala
+++ src/main/scala/shading/material/EmissiveMaterial.scala
@@ -5,10 +5,10 @@ import com.beardedplatypus.world.Scene
 
 class EmissiveMaterial(val ls: Double,
                        val c: Color) extends Material(false) {
-
+  val Le = c * ls
 
   override def shade(rayResult: RayResult, scene: Scene): Color  = {
-    if ((rayResult.normal dot rayResult.ray.direction.inverted) >= 0.0) c * ls
+    if ((rayResult.normal dot rayResult.ray.direction.inverted) >= 0.0) Le
     else Color.black
   }
 }
