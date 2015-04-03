f5a529445a232479753b68595301c03fd3e76a56
BeardedPlatypus <mwtegelaers@gmail.com>
2015-04-03 20:44:24 +0200

    Update Light to support AreaLights
    * Added the geometric term G(pHit, pSample)
    * Added inverse probability density function (1 / pdf)
    
    * Changed name of castsShadow to resolve conflict with naming in
      GeometricObject class

--- src/main/scala/world/light/Light.scala
+++ src/main/scala/world/light/Light.scala
@@ -1,27 +1,33 @@
 package com.beardedplatypus.world.light
 
 import com.beardedplatypus.math.{Point3d, Vector3d}
-import com.beardedplatypus.shading.Color
+import com.beardedplatypus.shading.{RayResult, Color}
 
 trait Light {
-  def L(hitPoint: Point3d): Color
-  def direction(hitPoint: Point3d): Vector3d
-  def distanceTo(hitPoint: Point3d): Double
-  val castsShadow: Boolean
+  def L(rayResult: RayResult, direction: Vector3d): Color
+  def G(pHit: Point3d, pSample: Point3d): Double = 1.0
+  def pdfDiv(pSample: Point3d) = 1.0
+
+  def sample(hitPoint: Point3d): List[LightSample]
+
+  val castsShadowLight: Boolean
 }
 
 class AmbientLight(val color: Color, val strength: Double) extends Light {
-  override def L(hitPoint: Point3d): Color = this.color * this.strength
-  override def direction(hitPoint: Point3d): Vector3d = throw new NotImplementedError()
-  override def distanceTo(hitPoint: Point3d): Double = throw new NotImplementedError()
-  override val castsShadow: Boolean = false
+  override def L(rayResult: RayResult, direction: Vector3d): Color = this.color * this.strength
+  override def sample(hitPoint: Point3d): List[LightSample] = throw new NotImplementedError()
+
+  override val castsShadowLight: Boolean = false
 }
 
 class DirectionLight(val color: Color, val strength: Double, val dir: Vector3d, shadow: Boolean) extends Light {
-  override def L(hitPoint: Point3d): Color = this.color * this.strength
-  override def direction(hitPoint: Point3d): Vector3d = dir.inverted.normalized
-  override def distanceTo(hitPoint: Point3d): Double = Double.PositiveInfinity
-  override val castsShadow: Boolean = shadow
+  override def L(rayResult: RayResult, direction: Vector3d): Color = this.color * this.strength
+  override def sample(hitPoint: Point3d): List[LightSample] = samples
+
+  override val castsShadowLight: Boolean = shadow
+
+  private val direction = dir.inverted.normalized
+  private val samples = List(new LightSample(Point3d.NaN, direction, Double.PositiveInfinity))
 }
 
 class PointLight(val color: Color,
@@ -29,12 +35,14 @@ class PointLight(val color: Color,
                  val position: Point3d,
                  val distanceAttenuation: (Point3d, Point3d) => Double,
                  shadow: Boolean) extends Light {
-  override def L(hitPoint: Point3d): Color = this.color * this.strength * distanceAttenuation(this.position, hitPoint)
-  override def direction(hitPoint: Point3d): Vector3d = (position - hitPoint).normalized
-  override def distanceTo(hitPoint: Point3d): Double =  {
+  override def L(rayResult: RayResult, direction: Vector3d): Color = this.color * this.strength * distanceAttenuation(this.position, rayResult.worldHitPoint)
+  override def sample(hitPoint: Point3d) = List(new LightSample(position, direction(hitPoint), distanceTo(hitPoint)))
+
+  private def direction(hitPoint: Point3d): Vector3d = (position - hitPoint).normalized
+  private def distanceTo(hitPoint: Point3d): Double =  {
     val p: Vector3d = hitPoint - position
     Math.sqrt(p dot p)
   }
 
-  override val castsShadow: Boolean = shadow
+  override val castsShadowLight: Boolean = shadow
 }
