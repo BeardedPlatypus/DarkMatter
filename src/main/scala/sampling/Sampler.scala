abc94d98f01cb0bc5bf872fe746d0439e0b16dc9
BeardedPlatypus <mwtegelaers@gmail.com>
2015-04-30 12:58:14 +0200

    update sampler to provide 2d samples on unit square
    * Sampler2d object: x, y coordinate generated by the new Sampler

--- src/main/scala/sampling/Sampler.scala
+++ src/main/scala/sampling/Sampler.scala
@@ -4,36 +4,32 @@ import com.beardedplatypus.sampling.SamplerStrategy.SamplerStrategy
 
 import scala.util.Random
 
+object SamplerStrategy extends Enumeration {
+  type SamplerStrategy = Value
+  val Jittered, Constant, Random = Value
+}
+
+
 object Sampler {
-  def generateSamples(width: Int, height: Int,
-                      offset_width: Int, offset_height: Int,
-                      numSamplesRoot: Int, strategy: SamplerStrategy): IndexedSeq[Sample] = {
+  def generateSamples(numSamplesRoot: Int,
+                      strategy: SamplerStrategy): IndexedSeq[Sample2d] = {
     strategy match {
       case SamplerStrategy.Constant => {
         val stepSize: Double = 1.0 / numSamplesRoot
         val stepOffset: Double = 0.5 * stepSize
-        for (u <- offset_width to offset_width + width - 1;
-             v <- offset_height to offset_height + height - 1;
-             i <- 1 to numSamplesRoot;
-             j <- 1 to numSamplesRoot) yield new Sample(u.toDouble + i.toDouble * stepSize - stepOffset,
-                                                        v.toDouble + j.toDouble * stepSize - stepOffset,
-                                                        u, v)
+        for (i <- 0 to numSamplesRoot;
+             j <- 0 to numSamplesRoot) yield new Sample2d(i * stepSize + stepOffset,
+                                                          j * stepSize + stepOffset)
       }
       case SamplerStrategy.Jittered => {
         val stepSize: Double = 1.0 / numSamplesRoot
-        for (u <- offset_width to offset_width + width - 1;
-             v <- offset_height to offset_height + height - 1;
-             i <- 0 to (numSamplesRoot - 1);
-             j <- 0 to (numSamplesRoot - 1)) yield new Sample(u.toDouble + (i.toDouble + this.rand.nextDouble) * stepSize,
-                                                              v.toDouble + (j.toDouble + this.rand.nextDouble) * stepSize,
-                                                              u, v)
+        for (i <- 0 to (numSamplesRoot - 1);
+             j <- 0 to (numSamplesRoot - 1)) yield new Sample2d((i.toDouble + this.rand.nextDouble) * stepSize,
+                                                                (j.toDouble + this.rand.nextDouble) * stepSize)
       }
       case SamplerStrategy.Random => {
-        for (u <- offset_width to offset_width + width - 1;
-             v <- offset_height to offset_height + height - 1;
-             i <- 1 to (numSamplesRoot * numSamplesRoot)) yield new Sample(u.toDouble + this.rand.nextDouble(),
-                                                                           v.toDouble + this.rand.nextDouble(),
-                                                                           u, v)
+        for (i <- 1 to (numSamplesRoot * numSamplesRoot)) yield new Sample2d(this.rand.nextDouble(),
+                                                                             this.rand.nextDouble())
       }
     }
   }
@@ -41,7 +37,6 @@ object Sampler {
   private val rand: Random = new Random()
 }
 
-object SamplerStrategy extends Enumeration {
-  type SamplerStrategy = Value
-  val Jittered, Constant, Random = Value
+class Sample2d(val u: Double, val v: Double) {
+  override def toString = "Sample2d | u: " + u + " v: " + v
 }
