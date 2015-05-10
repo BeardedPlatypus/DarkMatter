13b310afd7758bcd21376682b88b1e4ea007398e
BeardedPlatypus <mwtegelaers@gmail.com>
2015-05-10 15:23:35 +0200

    [FEAT] Add mesh functionality for set of triangles
    * Mesh is a set of triangles that can be moved as one

--- src/main/scala/world/geometry/objects/polygon/Mesh.scala
+++ src/main/scala/world/geometry/objects/polygon/Mesh.scala
@@ -1,14 +1,30 @@
-package world.geometry.objects.polygon
+package com.beardedplatypus.world.geometry.objects.polygon
 
-import com.beardedplatypus.math.Ray
+import com.beardedplatypus.math.{Transformation, Ray}
 import com.beardedplatypus.shading.RayResult
 import com.beardedplatypus.shading.material.Material
-import com.beardedplatypus.world.geometry.acceleration_structures.AABBNode
-import world.geometry.objects.FiniteGeometricObject
+import com.beardedplatypus.world.geometry.acceleration_structures.{AABBTree, AccelerationStructure}
+import com.beardedplatypus.world.geometry.objects.FiniteGeometricObject
 
-class Mesh(triangleStructure: AABBNode,tMat: Material, tShadow: Boolean) extends FiniteGeometricObject(tMat, tShadow) {
-  override def intersect(ray: Ray): Option[RayResult] = boundingBox.intersect(ray)
-  override def intersectDistance(ray: Ray): Option[Double] = boundingBox.intersectDistance(ray)
+// TODO make triangleStructure more general
+class Mesh(triangleStructure: AABBTree, val transformation: Transformation, tMat: Material, tShadow: Boolean) extends FiniteGeometricObject(tMat, tShadow) {
+  override def intersect(ray: Ray): Option[RayResult] = {
+    val rayLocal = transformation transformInv ray
+    val intersectionOption = boundingBox.intersect(rayLocal)
 
-  lazy val boundingBox = triangleStructure
+    if (intersectionOption.isEmpty) None
+    else {
+      val intersection = intersectionOption.get
+      Option(RayResult(intersection.localHitPoint,
+                       transformation transform intersection.worldHitPoint,
+                       (transformation transformInvTranspose intersection.normal).normalized,
+                       intersection.uv,
+                       intersection.material,
+                       ray,
+                       intersection.distance))
+    }
+  }
+  override def intersectDistance(ray: Ray): Option[Double] = boundingBox.intersectDistance(transformation transformInv ray)
+
+  val boundingBox = triangleStructure.root
 }
